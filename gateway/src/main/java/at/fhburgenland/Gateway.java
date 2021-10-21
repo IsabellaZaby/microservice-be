376f957cbf3e2c7b833adb9d8a305a3efc11f8fc
waldnerk <2010859019@fh-burgenland.at>
2021-10-21 20:30:46 +0200

    added configuration for gateway

---
+++ gateway/src/main/java/at/fhburgenland/Gateway.java
@@ -0,0 +1,16 @@
+package at.fhburgenland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Gateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway.class, args);
+    }
+
+}
+
